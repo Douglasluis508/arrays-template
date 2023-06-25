8a2f6b8cf3e6805311dbe3a8f6c86b04df2906a5
Douglasluis508 <douglasluis508@gmail.com>
2023-06-25 14:37:32 -0300

    aula guiada de arrays

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-function imprimirArray (){
-  //digite a sua solução aqui
+// const arrayString =['lápis', 'caneta', 'tesoura'];
+// console.log(arrayString.length);
 
-}
+// const arrayNumerico = [1,3,4];
 
-imprimirArray()
+// const arrayMix = [1,'lápis', true];
+// console.log(arrayMix.includes('lápis'));
+
+// const arrayNumero = [1];
+// console.log(arrayNumero[0]);
+// console.log(arrayNumero.length);
+
+// const arrayVazio = []
+
+// const arrayStringCopia = [...arrayString];
+// console.log(arrayString);
+// arrayStringCopia.push('papel');
+// console.log(arrayStringCopia);
+
+// arrayStringCopia.splice(2,2)
+// console.log(arrayStringCopia);
+
+
+const arrayNumerico2 = [10,2,15,3,22];
+
+const novoArray = arrayNumerico2.slice();
+
+console.log(novoArray);
+
+novoArray.pop();
+console.log(novoArray);
+
+novoArray.push(6);
+console.log(novoArray);
+
+novoArray.splice(2,1);
+console.log(novoArray);
+
+console.log(arrayNumerico2);
+console.log(novoArray);
